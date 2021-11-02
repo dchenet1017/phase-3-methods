031835b48b8b501caa5dcb03afa7875bd6a0dd57
Dimitri Chenet <dchenet1017@gmail.com>
2021-11-02 08:36:37 -0400

    commit tree d0fede228092f66f6062bec1757de1c46dd73100

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+    end 
+    
+
+def greet(name)
+    puts "Hello, #{name}!"
+end 
+greet "Naureen"
+
+def greetWithDefault(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greetWithDefault "Naureen"
+greetWithDefault
+
+def add(num1, num2)
+     num1 + num2
+end 
+add(2,5)
